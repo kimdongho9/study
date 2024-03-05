f0c26568e1abb40a77fc283705c213dc95ee3afd
kimdongho9 <guzzi9393@gamil.com>
2024-03-06 00:10:11 +0900

    add

--- rockpaperscissor/src/App.js
+++ rockpaperscissor/src/App.js
@@ -7,29 +7,53 @@ import React, { useState } from 'react'
 const choice = {
   rock:{
     name:"Rock",
-    img:"https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png"
+    img:"https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png"
   },
   scissors:{
     name:"Scissors",
-    img:"https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png"
+    img:"https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png"
   },
   paper:{
     name:"paper",
-    img:"https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png"
+    img:"https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png"
+    
   }
 }
 //반드시 하나만 return됨.
 const App = () => {
-  const play=(userChoice)=>{
-    console.log("선택됨",userChoice)
-  }
+  const [userSelect,setUserSelect] = useState(null)
+  const [comSelect,setComSelect] = useState(null)
 
+  const play=(userChoice)=>{
+
+    // userSelect = choice[userChoice]요렇게는 안된다.
+
+    setUserSelect(choice[userChoice])
+    let comChoice = randomChoice()
+    setComSelect(comChoice)
+    
+  };
+
+  const randomChoice = () => {
+
+    // 객체에 키값만 뽑아서 어레이로 만들어주는 함수다.
+    let itemArray = Object.keys(choice);
+    console.log("item array",itemArray)
+
+    let randomItem = Math.floor(Math.random()*itemArray.length);
+    console.log("random value",randomItem)
+    
+    let final = itemArray[randomItem];
+    console.log("final",final)
+
+    return choice[final]
+  }
 
   return (
     <div>
       <div className='main'>
-        <Box title='YOU' />
-        <Box title='COM' />
+        <Box title='YOU' item={userSelect}/>
+        <Box title='COM' item={comSelect}/>
       </div>
         
 
--- rockpaperscissor/src/component/Box.js
+++ rockpaperscissor/src/component/Box.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
 const Box = (props) => {
+console.log("props",props)
   return (
     <div className='box'>
       <h1>{props.title}</h1>
-      <img className="item-img"            src="https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png" />
+      <img className="item-img"    src={props.item && props.item.img} />
 
-      {/* https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png */}
-
-      {/* https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png */}
       <h2>WIN</h2>
     </div>
   )

10affdbd453d64f89bf42b6887e97c7421dd29a1
kimdongho9 <guzzi9393@gamil.com>
2024-03-06 20:07:01 +0900

    add

--- rockpaperscissor/src/App.js
+++ rockpaperscissor/src/App.js
@@ -1,65 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
 import Box from "./component/Box"
 import React, { useState } from 'react'
 
 
 const choice = {
-  rock:{
-    name:"Rock",
-    img:"https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png"
+  rock: {
+    name: "Rock",
+    img: "https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png"
   },
-  scissors:{
-    name:"Scissors",
-    img:"https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png"
+  scissors: {
+    name: "Scissors",
+    img: "https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png"
   },
-  paper:{
-    name:"paper",
-    img:"https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png"
-    
+  paper: {
+    name: "Paper",
+    img: "https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png"
+
   }
 }
 //반드시 하나만 return됨.
 const App = () => {
-  const [userSelect,setUserSelect] = useState(null)
-  const [comSelect,setComSelect] = useState(null)
+  const [userSelect, setUserSelect] = useState(null)
+  const [comSelect, setComSelect] = useState(null)
+  const [result, setResult] = useState("")
 
-  const play=(userChoice)=>{
+  const play = (userChoice) => {
 
     // userSelect = choice[userChoice]요렇게는 안된다.
 
-    setUserSelect(choice[userChoice])
-    let comChoice = randomChoice()
-    setComSelect(comChoice)
-    
+    setUserSelect(choice[userChoice]);
+
+    //com의 가위바위보를 랜덤으로 나오게 함.
+    let comChoice = randomChoice();
+    setComSelect(comChoice);
+
+    //win인지 lose인지 나타냄.
+    setResult(judgement(choice[userChoice], comChoice));
+
   };
 
+  //유저가 이겼을 때 경우의 수들
+  const judgement = (user, com) => {
+    console.log("user", user, "computer", com)
+
+
+    if (user.name == com.name) {
+      return "tie"
+    } else if (user.name == "Rock")
+      return com.name == "Scissors" ? "win" : "lose"
+      else if (user.name == "Scissors")
+      return com.name == "Paper" ? "win" : "lose"
+      else if (user.name == "Paper")
+      return com.name == "Rock" ? "win" : "lose"
+  }
+
+
+    //랜덤으로 번호 나오게 하기(com)
   const randomChoice = () => {
 
     // 객체에 키값만 뽑아서 어레이로 만들어주는 함수다.
     let itemArray = Object.keys(choice);
-    console.log("item array",itemArray)
+    console.log("item array", itemArray)
 
-    let randomItem = Math.floor(Math.random()*itemArray.length);
-    console.log("random value",randomItem)
-    
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value", randomItem)
+
     let final = itemArray[randomItem];
-    console.log("final",final)
+    console.log("final", final)
 
     return choice[final]
   }
 
+
+
+
   return (
     <div>
       <div className='main'>
-        <Box title='YOU' item={userSelect}/>
-        <Box title='COM' item={comSelect}/>
+        <Box title='YOU' item={userSelect} result={result} />
+        <Box title='COM' item={comSelect} result={result} />
       </div>
-        
 
-      
+
+
       <div className='main'>
-         {/* 함수를 콜백형태로 전달해주는 것이 중요! */}
+        {/* 함수를 콜백형태로 전달해주는 것이 중요! */}
         <button onClick={() => play("scissors")}>가위</button>
         <button onClick={() => play("rock")}>바위</button>
         <button onClick={() => play("paper")}>보</button>
--- rockpaperscissor/src/component/Box.js
+++ rockpaperscissor/src/component/Box.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
+
 
 const Box = (props) => {
-console.log("props",props)
+  let result; // 결과를 담을 변수 선언
+
+  // 만약 props.title이 "COM"이고, 결과가 비긴 것이 아니며, props.result에 값이 있는 경우
+  if (
+    props.title === "COM" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    // 만약 결과가 "win"이면 "lose"로, 그렇지 않으면 "win"으로 설정
+    result = props.result === "win" ? "lose" : "win";
+  } else { // 위의 경우가 아니라면 props로 전달된 결과를 그대로 사용
+    result = props.result;
+  }
+
+  
+  if (props.title === "COM") {
+    console.log("com", result); 
+  }
+
+  console.log("props", props); 
+
+
+  // Box 컴포넌트의 렌더링
   return (
-    <div className='box'>
-      <h1>{props.title}</h1>
-      <img className="item-img"    src={props.item && props.item.img} />
-
-      <h2>WIN</h2>
+    <div className={`box ${result}`}> 
+      <h1>{props.title}</h1> 
+      <img className="item-img" src={props.item && props.item.img} /> 
+      <h2>{props.result}</h2> 
     </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box; 

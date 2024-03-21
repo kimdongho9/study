e73b8bd1d560e02adec1fad8674eb425ffc0b5d8
kimdongho9 <guzzi9393@gmail.com>
2024-03-21 21:57:21 +0900

    추가

--- rockpaperscissor/src/component/Box.js
+++ rockpaperscissor/src/component/Box.js
@@ -16,22 +16,28 @@ const Box = (props) => {
     result = props.result;
   }
 
-  
+
+
   if (props.title === "COM") {
-    console.log("com", result); 
+    console.log("com", result);
   }
 
-  console.log("props", props); 
+  console.log("props", props);
 
 
   // Box 컴포넌트의 렌더링
   return (
-    <div className={`box ${result}`}> 
-      <h1>{props.title}</h1> 
-      <img className="item-img" src={props.item && props.item.img} /> 
-      <h2>{props.result}</h2> 
+    <div className={`box ${result}`}>
+      <h1>{props.title}</h1>
+      <h2>{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+      <h2>{result}</h2>
     </div>
   );
 };
 
-export default Box; 
+export default Box;
+
+
+
+
